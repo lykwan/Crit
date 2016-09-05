6d0d9db8b7ac2b6e2db1746479fd02d5cc427cbf
Lily Kwan <lilyeekw@gmail.com>
2016-09-04 17:08:37 -0700

    fixed broken code by adding default next(action)

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,12 +3,14 @@ import SessionReducer from './session_reducer';
 import UserReducer from './user_reducer';
 import GroupReducer from './group_reducer';
 import EventReducer from './event_reducer';
+import EventResponseReducer from './event_response_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   users: UserReducer,
   groups: GroupReducer,
   events: EventReducer,
+  eventResponse: EventResponseReducer
 });
 
 export default RootReducer;
