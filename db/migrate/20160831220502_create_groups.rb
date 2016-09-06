6705047f4d8fe04aa81a6c3ad6ae29be7efc7d1b
App Academy Student <student@appacademy.io>
2016-09-06 16:02:00 -0700

    finished styling profile component

--- db/migrate/20160831220502_create_groups.rb
+++ db/migrate/20160831220502_create_groups.rb
@@ -3,6 +3,7 @@ class CreateGroups < ActiveRecord::Migration
     create_table :groups do |t|
       t.string :title, null: false, index: true
       t.text :description, null: false
+      t.string :img, null: false, default: "http://www.pawbuzz.com/wp-content/uploads/sites/551/2014/11/corgi-puppies-21.jpg"
       t.boolean :is_public, default: false
       t.timestamps null: false
     end
